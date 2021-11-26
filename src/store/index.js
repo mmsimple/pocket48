7765aa874a32e33705d27f99651e29d681041bca
simpleLiu <48751431+simple-1@users.noreply.github.com>
2021-11-27 00:18:35 +0800

    Add files via upload

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+import DatePeople from '../utils/data_people'
+
+export default createStore({
+  state: {
+    dataPeople: DatePeople()
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
