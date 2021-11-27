2bf5934d3919bf09c5566dc8f6a95d0c8f7790ac
simple <792378197@qq.com>
2021-11-27 14:27:55 +0800

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
+<script>
+export default {
+  name: "App",
+  mounted() {
+    /**
+     * 解决 css 引入图片在 github pages 无法获取的问题
+     */
+    const { NODE_ENV } = process.env
+    document.documentElement.className = NODE_ENV
+  },
+}
+</script>
 <style>
 body {
   margin: 0;
